02c1730e19b00c7e439b32af2cb4372453de0f25
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-02-29 23:02:07 +0530

    Update Custom.js

--- src/hooks/customHooks/Custom.js
+++ src/hooks/customHooks/Custom.js
@@ -6,17 +6,32 @@ const Custom = () => {
     function handleResize() {
       setWidth(window.innerWidth);
     }
+
     useEffect(() => {
       window.addEventListener("resize", handleResize);
-      console.log("calling resize event");
+      // console.log("calling resize event");
       return () => {
         window.removeEventListener("resize", handleResize);
       };
     }, []);
     return width;
   }
+
+  // callback
+  function calling(callback) {
+    console.log("calling callback");
+    callback();
+  }
+  function hello() {
+    console.log("hello");
+  }
+  calling(hello);
   const width = useWindowWidth();
-  return <div>{`The window width is : ${width}`}</div>;
+  return (
+    <div>
+      <h1>{`The window width is : ${width}`}</h1>
+    </div>
+  );
 };
 
 export default Custom;
