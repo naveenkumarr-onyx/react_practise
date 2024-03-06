4cdc58f4bd61dae5e367c99e8b28832100981877
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-03-07 00:13:30 +0530

    Create ProcessBar.jsx

---
+++ src/processBar/ProcessBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProcessBar = () => {
+  return (
+    <div className=" bg-red-400 flex h-[200px] justify-center items-center">
+      <div className=" flex ">
+        <div className=" bg-green-300 rounded-full w-[20px] h-[20px] flex justify-center items-center">
+          1
+        </div>
+        {/* <div>2</div>
+        <div>3</div>
+        <div>4</div> */}
+      </div>
+    </div>
+  );
+};
+
+export default ProcessBar;
