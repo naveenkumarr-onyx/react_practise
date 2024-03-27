302d7a2a8a537aee2b76bfa6ea282871d3e0c442
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-03-27 19:17:46 +0530

    Update Practise.js

--- src/Js/vanilla/Practise.js
+++ src/Js/vanilla/Practise.js
@@ -94,6 +94,20 @@ const Practise = () => {
     // console.log(p1.compareAge(p2));
     // console.log(p2.compareAge(p3));
     // console.log(p1.compareAge(p3));
+    var nums = [2, 7, 11, 15];
+    var target = 9;
+    function sum(nums, target) {
+      for (var i = 0; i < nums.length; i++) {}
+    }
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+
+    sum(nums, target);
   }, []);
 
   // console.log(drinks);
