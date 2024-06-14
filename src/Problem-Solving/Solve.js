a76d6dfafff12f8f8e0a41606cd0928443a701d5
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-06-14 19:35:08 +0530

    Update Solve.js

--- src/Problem-Solving/Solve.js
+++ src/Problem-Solving/Solve.js
@@ -2,24 +2,31 @@ import React, { useEffect } from "react";
 
 export const Solve = () => {
   useEffect(() => {
-    const subArraySum = (arr, n, s) => {
-      const result = [];
-      for (let i = 0; i < n; i++) {
-        let currentSum = arr[i];
-        if (currentSum === s) {
-          result.push([i, i]);
-        }
-        for (let j = i + 1; j < n; j++) {
-          currentSum += arr[j];
-          if (s === undefined) return -1;
-          if (currentSum === s) {
-            result.push([i, j]);
-          }
-        }
-      }
-      return result;
-    };
+    // const subArraySum = (arr, n, s) => {
+    //   const result = [];
+    //   for (let i = 0; i < n; i++) {
+    //     let currentSum = arr[i];
+    //     if (currentSum === s) {
+    //       result.push([i, i]);
+    //     }
+    //     for (let j = i + 1; j < n; j++) {
+    //       currentSum += arr[j];
+    //       if (s === undefined) return -1;
+    //       if (currentSum === s) {
+    //         result.push([i, j]);
+    //       }
+    //     }
+    //   }
+    //   return result;
+    // };
 
+    const subArraySum = (arr, n, s) => {
+      var currentSum = 0;
+      var start = 0;
+      for (let i = 0; i < n; i++) {
+        console.log((currentSum = currentSum + arr[i]));
+      }
+    };
     const array = [5, 3, 4];
     const targetSum = 2;
     const result = subArraySum(array, array.length, targetSum);
