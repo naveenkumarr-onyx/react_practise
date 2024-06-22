f87ae70cc42fcaffb90f01435a2df8f8ec4687c5
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-06-22 11:55:26 +0530

    Update Solve.js

--- src/Problem-Solving/Solve.js
+++ src/Problem-Solving/Solve.js
@@ -19,28 +19,39 @@ export const Solve = () => {
     //   }
     //   return result;
     // };
+    // const subArraySum = (arr, n, s) => {
+    //   var currentSum = 0;
+    //   var start = 0;
+    //   for (let i = 0; i < n; i++) {
+    //     currentSum = currentSum + arr[i];
+    //     // console.log((currentSum = currentSum + arr[i]));
+    //     while (currentSum > s && start <= i) {
+    //       currentSum = currentSum - arr[start];
+    //       start++;
+    //     }
+    //     if (currentSum === s) {
+    //       return [start + 1, i + 1];
+    //     }
+    //   }
+    //   return [-1];
+    // };
+    // const array = [1, 2, 3, 5, 6, 7, 8];
+    // const targetSum = 5;
+    // const result = subArraySum(array, array.length, targetSum);
+    // console.log(`result ${result}`);
 
-    const subArraySum = (arr, n, s) => {
-      var currentSum = 0;
-      var start = 0;
-      for (let i = 0; i < n; i++) {
-        currentSum = currentSum + arr[i];
-        // console.log((currentSum = currentSum + arr[i]));
-        while (currentSum > s && start <= i) {
-          currentSum = currentSum - arr[start];
-          start++;
-        }
-        if (currentSum === s) {
-          return [start + 1, i + 1];
-        }
-      }
-      return [-1];
-    };
+    function extractNumber(sentence) {
+      var numbers = sentence.match(/\d+/g);
+      var filteredNumbers = numbers
+        ? numbers.filter((num) => !num.includes("9")).map(Number)
+        : [];
 
-    const array = [1, 2, 3, 5, 6, 7, 8];
-    const targetSum = 5;
-    const result = subArraySum(array, array.length, targetSum);
-    console.log(`result ${result}`);
+      if (filteredNumbers.length === 0) return -1;
+      return Math.max(...filteredNumbers);
+    }
+    const sentence = "Another input 9000";
+    const result = extractNumber(sentence);
+    console.log(result);
   }, []);
 
   return <div className="font-bold">Problem-Solving</div>;
