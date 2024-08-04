d9e519c8e5aa97293e14cd5780f9662b7b263a8d
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-08-04 22:36:28 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(
+    const db = await mongoose.connect(
       "mongodb+srv://hda689747:naveen123@cluster0.ezbkydf.mongodb.net/?retryWrites=true&w=majority"
     );
-    console.log("DB CONNECTED");
+    if (db.Connection) {
+      console.log("MongoDB Connected");
+    }
+    if (db.disconnect()) {
+      console.log("db disconnected");
+    }
   } catch (error) {
     console.error(error.message);
   }
